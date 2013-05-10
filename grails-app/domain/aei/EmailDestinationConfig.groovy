2d654dad84f2e3a9e1d50c2628de840fa2c01a36
Sebastian Sierra <sebastian@tarmacit.com>
2013-05-10 16:04:47 -0300

    Added constraints to EmailDestinationConfig

--- grails-app/domain/aei/EmailDestinationConfig.groovy
+++ grails-app/domain/aei/EmailDestinationConfig.groovy
@@ -5,8 +5,17 @@ class EmailDestinationConfig extends DestinationConfig {
     String from
     String to
     String subject
-    String text
+    String body
 
     static constraints = {
+      from email: true
+      to email: true
+      subject nullable: false
+      body nullable: false
+    }
+
+    def beforeInsert () {
+      if (from == null)
+        from = "test@tarmac.com"
     }
 }
