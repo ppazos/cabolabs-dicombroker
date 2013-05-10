8cfef10196a6b927c0604218c4a57893917af541
Sebastian Sierra <sebastian@tarmacit.com>
2013-05-10 15:54:31 -0300

    Added destination domain-class and subclasses

---
+++ grails-app/domain/aei/EmailDestinationConfig.groovy
@@ -0,0 +1,12 @@
+package aei
+
+class EmailDestinationConfig extends DestinationConfig {
+
+    String from
+    String to
+    String subject
+    String text
+
+    static constraints = {
+    }
+}
