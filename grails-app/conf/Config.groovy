dd14a7633ee6ca3ece080095839e4956632448cf
Pablo Pazos <pablo.pazos@cabolabs.com>
2015-03-25 00:35:16 -0300

    removerd url for development

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -68,7 +68,7 @@ environments {
         grails.serverURL = "http://www.changeme.com"
     }
     development {
-        grails.serverURL = "http://localhost:8080/${appName}"
+        //grails.serverURL = "http://localhost:8080/${appName}"
     }
     test {
         grails.serverURL = "http://localhost:8080/${appName}"
